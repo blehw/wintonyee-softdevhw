6b4d6144dbac1ea2c4fb784e35564c9e8b4106dd
Winton Yee <wyee@github.com>
2016-04-11 09:41:01 -0400

    pushing

--- hw08/lc.py
+++ hw08/lc.py
@@ -1,4 +1,25 @@
 def union(a,b):
-    return [x for x in a]
+    return [x for x in a if x not in b] + b
 
 print union([1,2,3],[2,3,4])
+
+def intersection(a,b):
+    return [x for x in a if x in b]
+
+print intersection([1,2,3],[2,3,4])
+
+def setdif(a,b):
+    return [x for x in a if x not in b]
+
+print setdif([1,2,3],[2,3,4])
+
+def symdef(a,b):
+    return setdif(union(a,b),intersection(a,b))
+
+print symdef([1,2,3],[2,3,4])
+
+def cartprod(a,b):
+    return [(x,y) for x in a for y in b]
+
+print cartprod([1,2,3],[2,3,4])
+
