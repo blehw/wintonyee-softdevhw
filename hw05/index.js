16a823ff5e89c6139c88f5af0056951f91cd0824
unknown <winton2095@gmail.com>
2016-04-12 20:46:53 -0400

    diagnostic stuff

--- hw05/index.js
+++ hw05/index.js
@@ -1,37 +1,13 @@
-var states = ["Iowa","N.H.","Nev.","S.C.","Ala.","Ark.","Colo.","Ga","Mass.","Minn.","Okla.","Tenn.","Tex.","Vt.","Va.","A. Samoa","Kan.","La.","Neb.","Me.","Mich.","Miss.","Northern Mariana Islands","Fla.","Ill.","Mo.","N.C.","Ohio"];
+var candidates = ["Hillary Clinton","Ted Cruz","Bernie Sanders","Marco Rubio","Ben Carson","Donald J. Trump","John Kasich","Jeb Bush","Scott Walker","Carly Fiorina","Chris Christie","Rand Paul","Rick Perry","Bobby Jindal","Lindsey Graham","Mike Huckabee","Martin O'Malley","George Pataki","Rick Santorum","Lawrence Lessig","Jim Webb","Lincoln Chafee","Jim Gilmore"]
 
-var clinton = [23,9,20,39,44,22,28,73,46,31,17,44,147,0,62,4,9,37,10,9,60,30,4,133,68,32,32,59,79];
+var money = [188.0,104.2,96.3,84.6,68.0,27.3,27.3,157.6,39.5,26.0,26.0,20.6,16.7,11.0,10.0,8.8,7.1,2.1,2.0,1.2,0.8,0.4,0.2]
 
-d3.select(".clinton")
+d3.select(".money")
 .selectAll("div")
-.data(clinton)
+.data(candidates).
+text(function(d) {return d;});
+
+/*.data(money)
 .enter().append("div")
 .style("width",function(d) {return d * 3 + "px";})
-.text(function(d) {return d;});
-
-/*
-d3.select(".clinton")
-.selectAll("div")
-.each()
-.data(states)
-.enter().append("div")
-.text(function(d) {return d;});
-*/
-
-var sanders = [21,15,15,14,9,10,38,29,45,46,21,23,75,16,33,2,24,14,15,16,67,4,2,65,67,32,45,62];
-
-d3.select(".sanders")
-.selectAll("div")
-.data(sanders)
-.enter().append("div")
-.style("width",function(d) {return d * 3 + "px";})
-.text(function(d) {return d;});
-
-var unassigned = [85,27,37,20,34,118,96,18,291,70,31,118,210,33,92,12,37,61,74,12,67,546,27,142,43,23,25,45];
-
-d3.select(".unassigned")
-.selectAll("div")
-.data(unassigned)
-.enter().append("div")
-.style("width",function(d) {return d * 3 + "px";})
-.text(function(d) {return d;});
+.text(function(d) {return d;});*/
---
+++ hw10/backEnd.py
@@ -0,0 +1,114 @@
+import urllib2, google, bs4, re
+
+from diagnostic import name_log, timer
+
+#@name_log
+@timer
+def search(query):
+    """
+    return result of google search
+    
+    :param query: a string
+    
+    :retval: array of first 10 google results from search
+    """
+    result=google.search(query,num=10,start=0,stop=10)
+    links=[]
+    for link in result:
+        links.append(link)
+    return links
+
+#@name_log
+@timer
+def get_page(link):
+    """
+    return contents of web page at link
+    
+    :param link: a string
+    
+    :retval: string of page contents
+    """
+    try:
+        url = urllib2.urlopen(link)
+        page = url.read()
+        soup = bs4.BeautifulSoup(page,'html')
+        raw = soup.get_text()
+        return raw
+    except:
+        return "error"
+
+#@name_log
+@timer
+def search_page(text,query):
+    """
+    return occurences of desired information in a text
+    
+    :param text: a string
+    :param query: a string. Will be searched to determine what we are looking for
+    
+    :retval: dictionary: Keys are names/locations/dates, their values are how often they occur in the text
+    """
+    #Determine if this is a who,where or when query
+    if re.search("(W|w)ho",query):
+        exp = "[A-Z][a-z]+ [A-Z][a-z]+"
+    elif re.search("(W|w)hen",query):
+        exp= "([0-9]{1,4}/[0-9]{1,4}/[0-9]{1,4})|([A-Z][a-z]+ [0-9]{0,2}, [0-9]{0,4})"
+    elif re.search("(W|w)here",query):
+        exp= "(T|t)he [A-Z][a-z]+ ?([A-Z][a-z]+ )+"
+    else:
+        raise SystemExit(0)
+    result = re.finditer(exp,text)
+    ans = {}
+    for match in result:
+        name=match.group(0)
+        if name in ans:
+            ans[name]+=1;
+        else:
+            ans[name]=1;
+    return ans
+        
+#@name_log
+@timer    
+def find_max(dic):
+    """
+    return the data value that occurs most frequently in the dictionary
+    
+    :param dic: a dictionary with strings as keys, their values as ints
+    
+    :retval: The value that occurs the most. Unclear which value it returns if two keys share the same value
+    """
+    #print dic
+    occurences=list(dic.values())
+    data=list(dic.keys())
+    #print max(occurences)
+    return data[occurences.index(max(occurences))]
+
+@name_log
+@timer
+def find_answer(query):
+    """
+    return the internet's answer to the query
+    
+    :param query: a string
+    
+    :retval: The answer that appears most often in the first 10 links of the google search of the question
+    """
+    links=search(query)
+    master_results={}
+    for link in links:
+        names=search_page(get_page(link),query)
+        for name in names:
+            if name in master_results:
+                master_results[name]+=1;
+            else:
+                master_results[name]=1;
+        #print master_results
+    return find_max(master_results)
+
+#print find_answer("Who invented the lightbulb?")
+#print find_answer("Who was secretary of state for FDR?")
+#print find_answer("When did lincoln die?")
+#print find_answer("Where was lincoln shot?")
+#print find_answer("Where does Obama live?")
+#print find_answer("when did the nazis surrender?")
+
